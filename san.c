6f509a1640f90c1f190c47e94f7d5a88456936ae
santhosh921 <43337965+santhosh921@users.noreply.github.com>
2019-01-31 14:03:35 +0530

    Create san.c

---
+++ san.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the values:");
+scanf("%d %d %d",&a &b &c);
+if(a>b)&&(a>c)
+printf(a);
+else if(b>c)
+printf(b);
+else
+printf(c);
+getch();
+}
